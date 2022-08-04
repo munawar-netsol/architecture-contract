ce7d00e89d04f31562a46818efe4607eed5d48b1
unknown <munawersheikh@gmail.com>
2022-08-04 07:40:19 +0500

    1st commit

--- ContractModificationService/Program.cs
+++ ContractModificationService/Program.cs
@@ -20,41 +20,69 @@ builder.Services.AddMassTransit(x =>
     x.AddConsumer<CreateContractConsumer>(typeof(CreateContractConsumerDefinition));
     //x.AddConsumersFromNamespaceContaining<PublishContractConsumer>();
 
-    x.UsingRabbitMq((context, cfg) =>
+    //x.UsingRabbitMq((context, cfg) =>
+    //{
+
+    //    //cfg.AutoStart = true;
+
+    //    if (builder.Configuration.GetSection("MassTransit")["host"] == "localhost")
+    //        cfg.Host("localhost", "/", h => {
+    //            h.Username("guest");
+    //            h.Password("guest");
+    //        });
+    //    else
+    //        cfg.Host("rabbitmq", "/", h => {
+    //            h.Username("guest");
+    //            h.Password("guest");
+    //        });
+
+    //    cfg.ReceiveEndpoint("priority-orders-receive", x =>
+    //    {
+    //        x.ConfigureSubscriberContract<PublishContractConsumer>(context, "priority-orders", "submitorder", "PRIORITY", ExchangeType.Direct);
+    //    });
+
+    //    cfg.ReceiveEndpoint("regular-orders-receive1", x =>
+    //    {
+    //        x.ConfigureSubscriberContract<PublishContractConsumerTwo>(context, "priority-orders", "submitorder", "REGULAR", ExchangeType.Direct);
+    //    });
+
+    //    cfg.ReceiveEndpoint("regular-orders-receive2", y =>
+    //    {
+    //        y.ConfigureSubscriberContract<CreateContractConsumer>(context, "regular-orders", "createorder", "REGULAR", ExchangeType.Direct);
+    //    });
+
+    //    cfg.ConfigurePublisherContracts();
+    //    cfg.ConfgurePublisherContract<ContractSubmitMessageEnvelop>("submitorder", ExchangeType.Direct);
+    //    cfg.ConfgurePublisherContract<ContractCreateMessageEnvelop>("createorder", ExchangeType.Direct);
+    //    cfg.ConfigureEndpoints(context);
+    //});
+    builder.Services.AddMassTransit(x =>
     {
-
-        //cfg.AutoStart = true;
-
-        if (builder.Configuration.GetSection("MassTransit")["host"] == "localhost")
-            cfg.Host("localhost", "/", h => {
-                h.Username("guest");
-                h.Password("guest");
+        if (builder.Configuration.GetSection("MassTransit")["Type"] == "rabbitmq")
+        {
+            x.UsingRabbitMq((context, cfg) =>
+            {
+                cfg.AutoStart = true;
+                    cfg.Host(builder.Configuration.GetSection("MassTransit")["host"], "/", h =>
+                    {
+                        h.Username("guest");
+                        h.Password("guest");
+                    });
+                cfg.ConfigureEndpoints(context);
             });
-        else
-            cfg.Host("rabbitmq", "/", h => {
-                h.Username("guest");
-                h.Password("guest");
+        }
+        else if (builder.Configuration.GetSection("MassTransit")["Type"] == "AWS")
+        {
+            x.UsingAmazonSqs((context, cfg) =>
+            {
+                cfg.Host("us-east-1", h => {
+                    h.AccessKey("AKIAXERWNUJ2EAHW33UP");
+                    h.SecretKey("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");
+                });
+
+                cfg.ConfigureEndpoints(context);
             });
-
-        cfg.ReceiveEndpoint("priority-orders-receive", x =>
-        {
-            x.ConfigureSubscriberContract<PublishContractConsumer>(context, "priority-orders", "submitorder", "PRIORITY", ExchangeType.Direct);
-        });
-
-        cfg.ReceiveEndpoint("regular-orders-receive1", x =>
-        {
-            x.ConfigureSubscriberContract<PublishContractConsumerTwo>(context, "priority-orders", "submitorder", "REGULAR", ExchangeType.Direct);
-        });
-
-        cfg.ReceiveEndpoint("regular-orders-receive2", y =>
-        {
-            y.ConfigureSubscriberContract<CreateContractConsumer>(context, "regular-orders", "createorder", "REGULAR", ExchangeType.Direct);
-        });
-
-        cfg.ConfigurePublisherContracts();
-        cfg.ConfgurePublisherContract<ContractSubmitMessageEnvelop>("submitorder", ExchangeType.Direct);
-        cfg.ConfgurePublisherContract<ContractCreateMessageEnvelop>("createorder", ExchangeType.Direct);
-        cfg.ConfigureEndpoints(context);
+        }
     });
 });
 var app = builder.Build();
